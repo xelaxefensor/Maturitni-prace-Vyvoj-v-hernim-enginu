740cd5d5638475bf68197b51e42ee171d7843d8a
Xela <alextl461@gmail.com>
2023-10-25 14:07:28 +0200

    CAM

--- Maturitní práce - Hra/PlayerCamera.gd
+++ Maturitní práce - Hra/PlayerCamera.gd
@@ -3,13 +3,23 @@ extends Camera2D
 var maxZoomOut = Vector2(500,300)
 var target
 var lookAhead = Vector2(0.8,0.3)
+var speed = 5
+
+var placeHolder
+var border = Vector2(150,150)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	target = self.get_node("..")
+	target = self.get_node("../Player")
+	placeHolder = target.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_action_pressed("camera_focus")):
-		position = (-get_viewport().size / 2.0 + get_viewport().get_mouse_position()).clamp(-maxZoomOut,maxZoomOut) 
+		position = (-get_viewport().size / 2.0 + get_viewport().get_mouse_position()).clamp(-maxZoomOut,maxZoomOut) + target.global_position
 	else:
-		position = Vector2(target.velocity * lookAhead)
+		if (Vector2(target.global_position.x,0).distance_to(Vector2(placeHolder.x,0)) >  border.x):
+			position.x = target.position.x + target.velocity.x * lookAhead.x
+			placeHolder.x = target.global_position.x - border.x * ((target.global_position.x-placeHolder.x)/abs(target.global_position.x-placeHolder.x))
+		if (Vector2(0,target.global_position.y).distance_to(Vector2(0,placeHolder.y)) >  border.y):
+			position.y = target.position.y + target.velocity.y * lookAhead.y
+			placeHolder.y = target.global_position.y - border.y * ((target.global_position.y-placeHolder.y)/abs(target.global_position.y-placeHolder.y))
