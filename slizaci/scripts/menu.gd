8e515377a1bdf2446fbd64a8681a4af176807b55
Xela <72282274+SkeletoMukit@users.noreply.github.com>
2023-12-13 15:43:43 +0100

    menu fix

--- slizaci/scripts/menu.gd
+++ slizaci/scripts/menu.gd
@@ -10,33 +10,40 @@ func _ready():
 
 
 func _on_play_pressed():
-	get_node("StartMenu").visible = false
+	menus_invisible()
 	get_node("MPMenu").visible = true
 
 
 func _on_back_pressed():
-	get_node("MPMenu").visible = false
+	menus_invisible()
 	get_node("StartMenu").visible = true
 
 
 func _on_exit_pressed():
 	get_tree().quit()
+	
+	
+func menus_invisible():
+	var children : Array = get_children()
+	for i in children.size():
+		children[i].visible = false
 
 
 func _on_connect_pressed():
 	if get_node("MPMenu/Address").text.is_empty():
 		return
 	connect_client.emit(get_node("MPMenu/Address").text)
-	get_node("MPMenu").visible = false
+	menus_invisible()
 	$ConnectingMenu.visible = true
 	
+	
 func player_connected(_id, _info):
-	$ConnectingMenu.visible = false
+	menus_invisible()
 
 
 func _on_host_pressed():
 	host_client.emit()
-	get_node("MPMenu").visible = false
+	menus_invisible()
 
 
 func _on_player_name_text_changed(new_text):
@@ -44,13 +51,14 @@ func _on_player_name_text_changed(new_text):
 
 
 func failed_to_connect():
+	menus_invisible()
 	$FailedToConnectMenu.visible = true
 
 func _on_failed_to_connect_back_pressed():
-	$FailedToConnectMenu.visible = false
+	menus_invisible()
 	$MPMenu.visible = true
 
 
 func _on_abort_pressed():
-	$ConnectingMenu.visible = false
+	menus_invisible()
 	$MPMenu.visible = true
